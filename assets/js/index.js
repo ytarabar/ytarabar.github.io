b2917dc04845b3b7f193225c886602610547f647
ytarabar <73908751+ytarabar@users.noreply.github.com>
2022-12-07 17:11:39 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ var Typer = {
   init: function () {
     accessCountimer = setInterval(function () {
       Typer.updLstChr();
-    }, 500);
+    }, 0);
     $.get(Typer.file, function (data) {
       Typer.text = data;
       Typer.text = Typer.text.slice(0, Typer.text.length - 1);
